97ebe3bb4313fb891d9b3e5867e67b616c253f85
Anderson Benjamin <anderson_benjamin@hotmail.com>
2023-09-14 22:50:48 -0300

    Ate os 40 min de aula

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
+import { Github } from 'lucide-react'
+import { Button } from "@/components/ui/button"
+
+
 export function App() {
   return (
-    <h1>Hello World</h1>
+    <div  className="min-h-screen flex flex-col">
+      <div className="px-6 py-3 flex items-certer justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
+            Desenvolvido com amor
+          </span>
+          <Button variant="outline">
+             <Github className= "w-4 h4 mr-2"></Github>
+             Github
+          </Button>
+        </div>
+      </div>
+    </div>
   )
 }
 
